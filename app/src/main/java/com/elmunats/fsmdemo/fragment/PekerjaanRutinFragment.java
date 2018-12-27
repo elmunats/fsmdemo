1bf1168e964ff6ce022131b4fc9b6b93e764f7ad
elmunats <natsir995@gmail.com>
2018-12-27 17:11:30 +0700

    update

---
+++ app/src/main/java/com/elmunats/fsmdemo/fragment/PekerjaanRutinFragment.java
@@ -0,0 +1,60 @@
+package com.elmunats.fsmdemo.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elmunats.fsmdemo.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PekerjaanRutinFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+    public PekerjaanRutinFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_pekerjaan_rutin, container, false);
+
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        // Menambah marker di Alun-alun Demak - Jateng, Indonesia, dan menggerakan kamera.
+        LatLng demak = new LatLng(-6.894796, 110.638413);
+        googleMap.addMarker(new MarkerOptions().position(demak).title("www.elmunats.com"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(demak));
+    }
+}
